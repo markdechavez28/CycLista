5969d0788a1e0d62988fe0fb30e5c5cdeeef2c7e
Mark Jerome De Chavez <146142700+markdechavez@users.noreply.github.com>
2025-03-05 16:38:30 +0800

    Added Vercel files

--- bike_counter/settings.py
+++ bike_counter/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-&##+i_-_0ebbfy=to&tix*vap+*kekx8exuko)t6is018m$g_d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+import os
+
+ALLOWED_HOSTS = ["your-vercel-project.vercel.app", "127.0.0.1"]
+
 
 # Render deployment configuration: disable DEBUG and configure static files when on Render
 if os.environ.get("RENDER"):
@@ -46,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,6 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
